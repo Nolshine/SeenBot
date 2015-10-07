fd2e184fe2a0196ef6691ee36eb0ef31f97dbacd
Jeff Chapman II <jac@jac2.net>
2015-10-07 18:02:57 -0400

    Cleanup debug output in poryseer, flush stdout

--- poryseer.py
+++ poryseer.py
@@ -5,9 +5,10 @@ import SEENBOT
 
 #### CONFIG ####
 config = {}
-config["nick"] = "SolSeer"
+config["network"] = "esper"
+config["nick"] = "porygit"
 config["channel"] = "#3dpe"
-config["debug"] = False
+config["debug"] = True
 
 def debug(text):
     if config["debug"]:
@@ -17,12 +18,14 @@ seenBot = SEENBOT.SEENBOT() # initialize the bot
 
 for line in iter(sys.stdin.readline, b''):
 # strip network name
-    line = ' '.join(line.split(' ')[1:])
+    line = ' '.join(line[:-1].split(' ')[1:])
 
     if ("PRIVMSG" in line) or ("NICK" in line):
-        console = "\n-X- ATTENTION -X- " + line + "\n"
+        debug(config["nick"] + " in: " + line)
         seen = seenBot.process(line, config["nick"])
         if seen != None:
-            print "esper PRIVMSG " + config["channel"] + " :" + seen
-        debug(console)
+            ircMessage = config["network"] + " PRIVMSG " + config["channel"] + " :" + seen
+            debug(config["nick"] + " out: " + ircMessage)
+            print ircMessage
+            sys.stdout.flush()
 
