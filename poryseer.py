d15a8d20721585d557d88efa95adc8e2b4bd0c88
Jeff Chapman II <jac@jac2.net>
2015-10-07 17:10:29 -0400

    Add basic jitro compatible version, a few test files

---
+++ poryseer.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python2
+import sys
+import SEENBOT
+
+
+#### CONFIG ####
+config = {}
+config["nick"] = "SolSeer"
+config["channel"] = "#3dpe"
+config["debug"] = False
+
+def debug(text):
+    if config["debug"]:
+        print text
+
+seenBot = SEENBOT.SEENBOT() # initialize the bot
+
+debug("connecting")
+
+for line in iter(sys.stdin.readline, b''):
+# strip network name
+    line = ' '.join(line.split(' ')[1:])
+
+    if ("PRIVMSG" in line) or ("NICK" in line):
+        console = "\n-X- ATTENTION -X- " + line + "\n"
+        seen = seenBot.process(line, config["nick"])
+        if seen != None:
+            print "esper PRIVMSG " + config["channel"] + " :" + seen
+        debug(console)
+
