43042f814016dd391833261abd8ea007285f55c9
Ariel M Shaked <mtr131@gmail.com>
2015-10-09 20:03:30 +0100

    utf-8 encoding (broken)

--- SEENBOT.py
+++ SEENBOT.py
@@ -13,6 +13,8 @@ class DATA_CELL(object):
         self.nick_history = []
         self.recent_timestamp = timestamp
         self.memos = []
+        self.message_light = False # this here is a flag that turns on if a person is given a memo.
+                                   # when the person is seen the bot will inform them they have memos and turn this flag off.
     @staticmethod
     def load(s):
         dc = DATA_CELL(s['current_nick'], s['recent_timestamp'])
@@ -20,6 +22,12 @@ class DATA_CELL(object):
             dc.nick_history.append(nick)
         for memo in s['memos']:
             dc.memos.append(memo)
+        if s['message_light'] == 'true':
+            sys.stderr.write("string 'true' loads correcly")
+            dc.message_light = True
+        elif s['message_light'] == True:
+            sys.stderr.write("actual boolean value loads correctly")
+            dc.message_light = True
         return dc
 
 class SEENBOT(object):
@@ -73,29 +81,9 @@ class SEENBOT(object):
 
         for cell in self.database:
             if (cell.current_nick == nick) or (nick in cell.nick_history):
-                if cell.memos != []:
+                if cell.memos != [] and cell.message_light:
                     outgoing.append(cell.current_nick + ": You have memos!")
-                    pb_api_paste_code = ""
-                    for memo in cell.memos:
-                        msg = memo[0] + " - " + memo[1] + " said to you: " + memo[2] + "\n"
-                        msg = msg.decode('utf-8', 'ignore')
-                        sys.stderr.write(msg)
-                        pb_api_paste_code += msg
-                    sys.stderr.write(pb_api_paste_code)
-
-                    # this portion will convert memo list to a pastebin post
-                    url = "http://pastebin.com/api/api_post.php"
-                    pb_payload = {}
-                    pb_payload["api_dev_key"] = pb_api_dev_key
-                    pb_payload["api_option"] = "paste"
-                    pb_payload["api_paste_private"] = "1" # paste an unlisted paste
-                    pb_payload["api_paste_expire"] = "1H" # expire new paste in one hour
-                    pb_payload["api_paste_code"] = pb_api_paste_code
-                    r = requests.post(url, pb_payload)
-                    outgoing.append("Your link: " + r.text)
-
-                cell.memos = []
-                self.save()
+                    cell.message_light = False
 
         if data[1] == "privmsg":
             if len(data) >= 4:
@@ -130,12 +118,43 @@ class SEENBOT(object):
                                 if (target == cell.current_nick) or (target in cell.nick_history):
                                     memo = string.join(data[5:])
                                     cell.memos.append((timestamp, nick, memo))
-                                    sys.stderr.write(timestamp + ": Added memo: " + memo + " to CELL: " + cell.current_nick + "\n")
+                                    sys.stderr.write(timestamp + ": Added memo: " + memo + " to CELL: " + cell.current_nick + "\n".decode('utf-8', 'ignore'))
                                     outgoing.append("I will tell them when I next see them.")
+                                    cell.message_light = True
                                     self.save()
                                     return outgoing
                             outgoing.append("I have not seen "+target+" yet.")
                             return outgoing
+                elif data[3] == ":!memos":
+                    if (nick + ": You have memos!") in outgoing:
+                        outgoing.remove(nick + ": You have memos!")
+                    for cell in self.database:
+                        if nick == cell.current_nick:
+                            if cell.memos != []:
+                                pb_api_paste_code = ""
+                                for memo in cell.memos:
+                                    msg = memo[0] + " - " + memo[1] + " said to you: " + memo[2] + "\n"
+                                    sys.stderr.write(msg+"\n".decode('utf-8', 'ignore'))
+                                    pb_api_paste_code += msg
+                                sys.stderr.write(pb_api_paste_code)
+
+                                # this portion will convert memo list to a pastebin post
+                                url = "http://pastebin.com/api/api_post.php"
+                                pb_payload = {}
+                                pb_payload["api_dev_key"] = pb_api_dev_key
+                                pb_payload["api_option"] = "paste"
+                                pb_payload["api_paste_private"] = "1" # paste an unlisted paste
+                                pb_payload["api_paste_expire"] = "1H" # expire new paste in one hour
+                                pb_payload["api_paste_code"] = pb_api_paste_code
+                                r = requests.post(url, pb_payload)
+                                outgoing.append(nick + ": Your link: " + r.text)
+                                cell.memos = []
+                                self.save()
+                                return outgoing
+                            else:
+                                outgoing.append(nick + ":You have no memos.")
+                                return outgoing
+
         if outgoing == []:
             return None
         else:
